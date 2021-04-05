d7968abe643a87534199c7fef1f213eefeb76c5f
Renan Thomaz <renantms@gmail.com>
2021-04-05 10:52:50 -0300

    Initial commit

---
+++ src/main/java/br/com/invillia/paymentdb/PaymentDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.invillia.paymentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/invillia/paymentdb/PaymentDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.invillia.paymentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
